0d395b2514c4ccc305fb963451b3701b25fa830a
sklearn21 <sklearn21@gmail.com>
2021-04-11 22:19:00 +0530

    Added User model.

--- authentication/models.py
+++ authentication/models.py
@@ -32,3 +32,24 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
+class User(AbstractBaseUser, PermissionsMixin):
+    """User model."""
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+        
+
+
+
+
